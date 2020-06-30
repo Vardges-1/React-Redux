3db93d2f765a5e2d7b1435dac0f0d84493442793
Vardges-1 <vardgesbaghdasaryan93@hotmail.com>
2020-06-30 15:24:46 +0200

    ist fertig

--- pizza-shop/src/components/hoc/with-resto-service.js
+++ pizza-shop/src/components/hoc/with-resto-service.js
@@ -1,20 +1,18 @@
 import React from 'react';
 import RestoServiceContext from '../resto-service-context';
-import RestoService from '../../services/resto-service';
 
-
-const WithRestoService = () => (Wrapped) => {
-    return (props) => {
-        return(
+const WithRestoService = () => (Wrapped) => { 
+    return (props) => {  
+        return (
             <RestoServiceContext.Consumer>
-               {
-                   (RestoService) => {
-                       return <Wrapped {...props} RestoService={RestoService} />
-                   }
-               } 
+                {
+                    (RestoService) => {
+                        return <Wrapped {...props} RestoService = {RestoService}/>
+                    }
+                }
             </RestoServiceContext.Consumer>
         )
     }
-}
+};
 
 export default WithRestoService;
